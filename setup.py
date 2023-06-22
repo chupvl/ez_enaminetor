73bdc603f121c56574a4d42acf23323318348529
chupvl <chupvl@gmail.com>
2023-06-22 12:37:59 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='ez_enaminetor',
     version='2023.6',
+    packages=['csv2parquet', 'csv2parquet_withProps', 'csv_splitter'],
     url='https://github.com/chupvl/ez_enaminetor',
     license='MIT',
     author='chupvl',
