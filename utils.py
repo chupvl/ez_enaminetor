780253647939012ced8f5c35ef03e22cdbe076bd
chupvl <chupvl@gmail.com>
2023-06-22 11:50:48 -0400

    major update

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
+import pyarrow as pa
+
 def read_ipc(filename):
-    #f = '/home/chupvl/PycharmProjects/ezenaminetor/tmp_ll_9.txt.ipc'
-    #tmp = pa.ipc.RecordBatchFileReader(filename)
     ipc_file = pa.ipc.open_file(filename)
     schema = ipc_file.schema
     print(schema)
