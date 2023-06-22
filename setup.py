e44fb81b782688a0dbce717d6289593890b29fb8
chupvl <chupvl@gmail.com>
2023-06-22 12:27:07 -0400

    changes to init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ez_enaminetor',
+    version='2023.6',
+    packages=[''],
+    url='https://github.com/chupvl/ez_enaminetor',
+    license='MIT',
+    author='chupvl',
+    author_email='chupvl@gmail.com',
+    description='Collection of modules for working with Enamine REAL files'
+)
