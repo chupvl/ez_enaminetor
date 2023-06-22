52ffd58ce691b2f588abf513d7562d90146da4b7
chupvl <chupvl@gmail.com>
2023-06-22 12:21:32 -0400

    changes to init

--- __init__.py
+++ __init__.py
@@ -6,3 +6,6 @@ __author__ = 'Vladimir Chupakhin'
 __email__ = 'chupvl@gmail.com'
 __status__ = 'Development'
 __date__ = '2023-06-22'
+
+from csv2parquet_withProps import convert_csv_to_parquet_withProps
+from csv2parquet import convert_csv_to_parquet
